05f5826c2a8abb6058bc44ea0e1839bdbbc97b19
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-10-28 15:12:53 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117061/latihan56/umurbarangantik/PBO210117061Latihan56UmurBarangAntik.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan56.umurbarangantik;
+
+/**
+ *Nama      : Fachrian Harits P
+ *Kelas     : IF-2
+ *NIM       : 10117061
+ *Deskripsi : Program untuk menampilksan nama dan umur
+ */ 
+public class PBO210117061Latihan56UmurBarangAntik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Radio rd = new Radio("Radio AM",234);
+        System.out.println("Nama barang Antik : " +rd.getName() );
+        rd.tampilUmur();
+    
+    }
+    
+}
---
+++ src/pbo2/pkg10117061/latihan56/umurbarangantik/Radio.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan56.umurbarangantik;
+
+/**
+ *Nama      : Fachrian Harits P
+ *Kelas     : IF-2
+ *NIM       : 10117061
+ *Deskripsi : Program untuk menampilksan nama dan umur
+ */ 
+public class Radio extends BarangAntik{
+    
+    private String name ;
+
+    public Radio(String name, int umur) {
+        super(umur);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
